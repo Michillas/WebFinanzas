679b0dad4dab08f411c3739ece8fbfbef2d514b3
Michillas <elrickimont@gmail.com>
2025-04-30 08:34:00 +0200

    style: centered grid column

--- webfinanzas-frontend/components/layout/sections/team.tsx
+++ webfinanzas-frontend/components/layout/sections/team.tsx
@@ -67,56 +67,58 @@ export const TeamSection = () => {
         </h2>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-8">
         {teamList.map(
           (
-            { imageUrl, firstName, lastName, positions, socialNetworks },
-            index
+        { imageUrl, firstName, lastName, positions, socialNetworks },
+        index
           ) => (
-            <Card
-              key={index}
-              className="bg-muted/60 dark:bg-card flex flex-col h-full overflow-hidden group/hoverimg"
+        <Card
+          key={index}
+          className={`bg-muted/60 dark:bg-card flex flex-col h-full overflow-hidden group/hoverimg ${
+            index === 0 ? "lg:col-start-2 xl:col-start-2" : ""
+          }`}
+        >
+          <CardHeader className="p-0 gap-0">
+            <div className="h-full overflow-hidden">
+          <Image
+            src={imageUrl}
+            alt=""
+            width={300}
+            height={300}
+            className="w-full aspect-square object-cover saturate-0 transition-all duration-200 ease-linear size-full group-hover/hoverimg:saturate-100 group-hover/hoverimg:scale-[1.01]"
+          />
+            </div>
+            <CardTitle className="py-6 pb-4 px-6">
+          {firstName}
+          <span className="text-primary ml-2">{lastName}</span>
+            </CardTitle>
+          </CardHeader>
+          {positions.map((position, index) => (
+            <CardContent
+          key={index}
+          className={`pb-0 text-muted-foreground ${
+            index === positions.length - 1 && "pb-6"
+          }`}
             >
-              <CardHeader className="p-0 gap-0">
-                <div className="h-full overflow-hidden">
-                  <Image
-                    src={imageUrl}
-                    alt=""
-                    width={300}
-                    height={300}
-                    className="w-full aspect-square object-cover saturate-0 transition-all duration-200 ease-linear size-full group-hover/hoverimg:saturate-100 group-hover/hoverimg:scale-[1.01]"
-                  />
-                </div>
-                <CardTitle className="py-6 pb-4 px-6">
-                  {firstName}
-                  <span className="text-primary ml-2">{lastName}</span>
-                </CardTitle>
-              </CardHeader>
-              {positions.map((position, index) => (
-                <CardContent
-                  key={index}
-                  className={`pb-0 text-muted-foreground ${
-                    index === positions.length - 1 && "pb-6"
-                  }`}
-                >
-                  {position}
-                  {index < positions.length - 1 && <span>,</span>}
-                </CardContent>
-              ))}
+          {position}
+          {index < positions.length - 1 && <span>,</span>}
+            </CardContent>
+          ))}
 
-              <CardFooter className="space-x-4 mt-auto">
-                {socialNetworks.map(({ name, url }, index) => (
-                  <Link
-                    key={index}
-                    href={url}
-                    target="_blank"
-                    className="hover:opacity-80 transition-all"
-                  >
-                    {socialIcon(name)}
-                  </Link>
-                ))}
-              </CardFooter>
-            </Card>
+          <CardFooter className="space-x-4 mt-auto">
+            {socialNetworks.map(({ name, url }, index) => (
+          <Link
+            key={index}
+            href={url}
+            target="_blank"
+            className="hover:opacity-80 transition-all"
+          >
+            {socialIcon(name)}
+          </Link>
+            ))}
+          </CardFooter>
+        </Card>
           )
         )}
       </div>
