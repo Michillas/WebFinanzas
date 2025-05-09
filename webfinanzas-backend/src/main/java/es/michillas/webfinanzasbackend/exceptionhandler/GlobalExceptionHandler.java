2406fe64d34e97f01c93771ab3a4affe661d73a6
Michillas <elrickimont@gmail.com>
2025-05-09 15:50:54 +0200

    feat: default messages for exceptions

--- webfinanzas-backend/src/main/java/es/michillas/webfinanzasbackend/exceptionhandler/GlobalExceptionHandler.java
+++ webfinanzas-backend/src/main/java/es/michillas/webfinanzasbackend/exceptionhandler/GlobalExceptionHandler.java
@@ -10,11 +10,11 @@ public class GlobalExceptionHandler {
     
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handlerArgumentException(IllegalArgumentException e) {
-        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<String>("BAD REQUEST", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
     public ResponseEntity<String> handlerRuntimeException(RuntimeException e) {
-        return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_GATEWAY);
+        return new ResponseEntity<String>("RUNTIME EXCEPTION", HttpStatus.BAD_GATEWAY);
     }
 }
