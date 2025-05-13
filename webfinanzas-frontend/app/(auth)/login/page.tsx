b74999cd430f3de9501b84602017b34bc434e072
Michillas <elrickimont@gmail.com>
2025-05-13 11:12:41 +0200

    feat: env for api url

--- webfinanzas-frontend/app/(auth)/login/page.tsx
+++ webfinanzas-frontend/app/(auth)/login/page.tsx
@@ -12,7 +12,7 @@ export default function LoginPage() {
     const [password, setPassword] = useState("")
 
     const handleLogin = async () => {
-        const response = await fetch("http://localhost:8080/auth/login", {
+        const response = await fetch(process.env.NEXT_PUBLIC_API_URL + "auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ username, password }),
