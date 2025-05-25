26e3828ed6c042c5fec2ec112d00bb61d47f51bb
Michillas <elrickimont@gmail.com>
2025-05-25 13:20:36 +0200

    feat: vercel analytics

--- webfinanzas-frontend/app/layout.tsx
+++ webfinanzas-frontend/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/layout/shared/navbar/navbar";
 import { ThemeProvider } from "@/components/layout/theme-provider";
+import { Analytics } from "@vercel/analytics/next"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -28,6 +29,7 @@ export default function RootLayout({
           <Navbar />
 
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
